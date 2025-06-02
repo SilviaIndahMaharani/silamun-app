a381f43991d9b4c0bb6887274baeef557a0282d7
SilviaIndahMaharani <silviaindah500@gmail.com>
2025-06-02 14:30:29 +0700

    update backend

--- server/main.py
+++ server/main.py
@@ -1,5 +1,22 @@
 from fastapi import FASTAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from torchvision.models.detection import ssdmobile_net_v3_large
+from torchvision.models.detection import ssdlite320_mobilnete_v3_large
+from torchvision.transforms import functional as F
+from PIL import Image
+import torch
+import json
+import io
+from fastapi.staticfiles import StaticFiles
+from torchvision.transforms import functional as F
 
+# Initialize the FastAPI app
+app = FastAPI()
+app.mount("/seagrass_image", StaticFiles(directory="seagrass_image"), name="seagrass_image")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for CORS
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
